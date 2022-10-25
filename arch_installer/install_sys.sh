f5fad51e667ecb216e56dc638a2ad4722c494b86
Ruben S. Garcia <rssanjuan2704@gmail.com>
2022-10-25 13:09:38 +0300

    Create partitions and install base system

--- arch_installer/install_sys.sh
+++ arch_installer/install_sys.sh
@@ -29,3 +29,120 @@ dialog --title "Choose your hard drive" --no-cancel --radiolist \
 	15 60 4 "${devices_list[@]}" 2>hd
 
 hd=$(cat hd) && rm hd
+
+default_size="8"
+dialog --no-cancel --inputbox \
+	"You need three partitions: Boot, Root and Swap \n\
+    The boot partition will be 512M \n\
+    The root partition will be the remaining of the hard disk \n\n\
+    Enter below the partition size (in Gb) for the Swap. \n\n
+    If you don't enter anything, it will default to ${default_size}G. \n" \
+	20 60 2>swap_size
+size=($cat swap_size) && rm swap_size
+
+[[ $size =~ ^[0-9]+$ ]] || size=$default_size
+
+dialog --no-cancel \
+	--title "!!! DELETE EVERYTHING !!!" \
+	--menu "Choose the way you'll wipe your hard disk $(hd)" \
+	15 60 4 \
+	1 "Use dd (wipe all disk)" \
+	2 "Use shred (slow & secure)"
+3 "No need - my hard disk is empty" 2>eraser
+
+hderaser=$(cat eraser)
+rm eraser
+
+function eraseDisk() {
+	case $1 in
+	1) dd if=/dev/zero of="$hd" status=progress 2>&1 |
+		dialog \
+			--title "Formatting $hd..." \
+			--progressbox --stdout 20 60 ;;
+	2) shred -v "$hd" |
+		dialog \
+			--title "Formatting $hd..." \
+			--progressbox --stdout 20 60 ;;
+	3) ;;
+	esac
+}
+
+eraseDisk "$hderaser"
+
+# Creating partitions
+boot_partition_type=1
+[[ "$uefi" == 0 ]] && boot_partition_type=4
+
+# Create the partitions
+
+#g - create non empty GPT partition table
+#n - create new partition
+#p - primary partition
+#e - extended partition
+#w - write the table to disk and exit
+
+partprobe "$hd"
+
+#shellcheck disable=SC2128
+fdisk "$hd" <<EOF
+g
+n
+
+
++512M
+t
+$boot_partition_type
+n
+
+
++${size}G
+n
+
+
+
+w
+EOF
+partprobe "$hd"
+
+# Add a suffix "p" in case with have a NVMe controller chip
+echo "$hd" | grep -E 'nvme' &>/dev/null && hd="${hd}p"
+
+mkswap "${hd}2"
+swapon "${hd}2"
+
+mkfs.ext4 "${hd}3"
+mount "${hd}3" /mnt
+
+if [ "$uefi" = 1 ]; then
+	mkfs.fat -F32 "${hd}1"
+	mkdir -p /mnt/boot/efi
+	mount "{hd}1" /mnt/boot/efi
+fi
+
+# Install Arch
+pacstrap /mnt base base-devel linux linux-firmware
+genfstab -U /mnt >>/mnt/etc/fstab
+
+# Persist values for the next script
+echo "$uefi" >/mnt/var_uefi
+echo "$hd" >/mnt/var_hd
+
+mv comp /mnt/comp
+
+curl https://raw.githubusercontent.com/sigfriedcub1990/arch_installer/master/install_chroot.sh >/mnt/install_chroot.sh
+
+arch-root /mnt bash install_chroot.sh
+
+rm /mnt/var_uefi
+rm /mnt/var_hd
+rm /mnt/install_chroot.sh
+
+dialog --title "To reboot or not to reboot?" --yesno \
+	"Congrats! Installation is done! \n\n
+    Do you want to reboot your computer?" 20 60
+
+response=$?
+case $response in
+0) reboot ;;
+1) clear ;;
+esac
